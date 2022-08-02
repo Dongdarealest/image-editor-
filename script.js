c24fb5db77de73560f1d69cddef66aaae2b71702
Dongdarealest <107338168+Dongdarealest@users.noreply.github.com>
2022-08-03 03:15:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -116,3 +116,4 @@ resetFilterBtn.addEventListener("click", resetFilter);
 saveImgBtn.addEventListener("click", saveImage);
 fileInput.addEventListener("change", loadImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
+// ok
